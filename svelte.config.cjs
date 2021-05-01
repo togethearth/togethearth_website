97790964b4300a6e6258469fbf14e09010eea8b1
Vocamen <47220168+vocamen@users.noreply.github.com>
2021-05-01 17:35:26 +0200

    new static config

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('@sveltejs/kit').Config} */
+const adapter = require('@sveltejs/adapter-static');
 module.exports = {
+	// kit: {
+	// 	// hydrate the <div id="svelte"> element in src/app.html
+	// 	adapter: require('@sveltejs/adapter-static')(),
+	// 	target: '#svelte'
+	// }
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: require('@sveltejs/adapter-static')(),
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
